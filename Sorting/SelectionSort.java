5308132022764bc76392e4b5e86304ea55f183d7
David Roy <24599987+DavidSRoy@users.noreply.github.com>
2020-12-04 09:30:40 -0800

    Update SelectionSort.java

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,49 @@
+package Sorting;
+
+public class SelectionSort {
+    public static void main(String args[]) {
+        int[] array = {5, 2 , 2, 3 ,121, 4322 ,0, -1 , -2 , -100};
+        System.out.print(array);
+        selectionSort(array);
+        //printArray(array); 
+    }
+
+    private static void selectionSort(int[] array) {
+        int minIndex;
+        
+        for (int i=0; i < array.length - 1; i++) {
+            minIndex = i;
+            for (int j= i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+
+                }
+            }
+
+            if (minIndex != i) {
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+
+            }
+        }
+    }
+
+    /*
+    private static void printArray(int[] array) {
+        for (int i: array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+    */
+
+
+    /*
+    private static void swap(int[] array, int left, int right) {
+        int temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+    }
+    */
+}
